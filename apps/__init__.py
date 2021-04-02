2d2852e2c87af48c7df5f9edc71c21c0acd83711
Wwx0410 <1162335221@qq.com>
2021-04-02 23:28:27 +0800

    :tada:项目初始化

---
+++ apps/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @File : __init__.py.py
+# @Author :WeiSanJin
+# @Time :2021/04/02 22:55
+# @Site :https://github.com/WeiSanJin
---
+++ extra_apps/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @File : __init__.py.py
+# @Author :WeiSanJin
+# @Time :2021/04/02 22:56
+# @Site :https://github.com/WeiSanJin
